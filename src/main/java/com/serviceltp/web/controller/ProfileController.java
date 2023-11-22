51382dac62964fd5ebff95aae46a81742a56fa2f
Alex Odobesteanu <alexandru.odobesteanu@gmail.com>
2023-11-22 22:54:25 +0200

    Added JsonView For Profile

--- src/main/java/com/serviceltp/web/controller/ProfileController.java
+++ src/main/java/com/serviceltp/web/controller/ProfileController.java
@@ -1,16 +1,35 @@
 package com.serviceltp.web.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.springframework.data.repository.init.ResourceReader.Type.JSON;
+
+
 @Controller
 public class ProfileController {
-    private String profileData;
+
+    private String profileData="""
+{
+  "data": {
+    "x": "1",
+    "y": "1",
+    "url": "http://url.com"
+  },
+  "event": "start",
+  "show": 1,
+  "id": 50
+}
+""";
+
 
     @GetMapping("/profile")
-    public String profile()
+    public String profile(Model model)
     {
+        model.addAttribute("profileData",profileData);
         return "profile";
     }
 }

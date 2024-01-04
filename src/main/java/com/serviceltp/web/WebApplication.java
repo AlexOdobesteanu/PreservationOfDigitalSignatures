c8675e8ba1628ac220b19c8c03cdcd1b3fbd96d7
Alex Odobesteanu <alexandru.odobesteanu@gmail.com>
2024-01-04 23:54:18 +0200

    Added JWT Authorization

--- src/main/java/com/serviceltp/web/WebApplication.java
+++ src/main/java/com/serviceltp/web/WebApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -14,6 +15,10 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@GetMapping("/")
---
+++ src/main/java/com/serviceltp/web/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.serviceltp.web.repository;
+
+import com.serviceltp.web.models.User;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+}

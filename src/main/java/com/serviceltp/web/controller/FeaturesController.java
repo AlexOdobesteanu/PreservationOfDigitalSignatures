889db7c7ca5ddeff5cd6dd68f7d56550665a7c30
Alex Odobesteanu <alexandru.odobesteanu@gmail.com>
2024-01-08 22:11:28 +0200

    Added SignatureAugm. to B-LTA

--- src/main/java/com/serviceltp/web/controller/FeaturesController.java
+++ src/main/java/com/serviceltp/web/controller/FeaturesController.java
@@ -1,12 +1,28 @@
 package com.serviceltp.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serviceltp.web.JwtService;
+import com.serviceltp.web.LoadedCerts;
 import com.serviceltp.web.PdfValidatorCustom;
 import com.serviceltp.web.ProfileGeneratorImpl;
 import com.serviceltp.web.base.*;
 
 import eu.europa.esig.dss.detailedreport.jaxb.XmlBasicBuildingBlocks;
+import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.DigestDocument;
+import eu.europa.esig.dss.model.FileDocument;
+import eu.europa.esig.dss.model.InMemoryDocument;
+import eu.europa.esig.dss.pades.PAdESSignatureParameters;
+import eu.europa.esig.dss.pades.signature.PAdESService;
+import eu.europa.esig.dss.service.SecureRandomNonceSource;
+import eu.europa.esig.dss.service.crl.OnlineCRLSource;
+import eu.europa.esig.dss.service.ocsp.OnlineOCSPSource;
+import eu.europa.esig.dss.service.tsp.OnlineTSPSource;
+import eu.europa.esig.dss.spi.x509.aia.DefaultAIASource;
+import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
@@ -15,6 +31,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.security.cert.CertificateException;
 import java.text.ParseException;
@@ -76,11 +94,89 @@ public class FeaturesController{
 
     @RequestMapping(value = "/PreservePO", method = RequestMethod.POST)
     @ResponseBody
-    public String preservePO(@RequestBody(required = false) PresPreservePOType req)
-    {
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = userDetails.getUsername();
-        return username;
+    public String preservePO(@RequestBody(required = false) PresPreservePOType req) throws Exception {
+        String reqId = req.getReqId();
+        String profileIdentifier = req.getPro();
+
+        ProfileGeneratorImpl implGen=new ProfileGeneratorImpl();
+        PresProfileType profileActive = implGen.getProfile("active");
+
+        if (!profileIdentifier.equals(profileActive.getPid()))
+        {
+            throw new Exception("Profile is not active");
+        }
+
+        //Decode from base64
+//        ObjectMapper mapper = new ObjectMapper();
+//        String decoded = mapper.convertValue("encoded HERE", String.class);
+
+        List<PresPOType> objectList = req.getPo();
+
+        for (PresPOType po: objectList) {
+            String binaryData = po.getBinaryData().getValue();
+            byte[] decodedBytes = Base64.getDecoder().decode(binaryData);
+            List<XmlBasicBuildingBlocks> blocks= PdfValidatorCustom.validateBytes(decodedBytes);
+            if(blocks!=null)
+            {
+                for(XmlBasicBuildingBlocks block:blocks)
+                {
+                    if(block.getType().toString()=="SIGNATURE")
+                    {
+                        if (block.getConclusion().getIndication().toString().equals("PASSED"))
+                        {
+                            // VALID SIGNATURE
+
+                            DSSDocument toExtendDocument = new InMemoryDocument(decodedBytes);
+                            PAdESSignatureParameters parameters = new PAdESSignatureParameters();
+                            parameters.setSignatureLevel(SignatureLevel.PAdES_BASELINE_LTA);
+
+
+                            CommonCertificateVerifier cv = new CommonCertificateVerifier();
+                            cv.setTrustedCertSources(LoadedCerts.getInstance().trustedCertSource);
+                            cv.setOcspSource(new OnlineOCSPSource());
+                            cv.setCrlSource(new OnlineCRLSource());
+                            cv.setAIASource(new DefaultAIASource());
+                            cv.addTrustedCertSources();
+
+                            PAdESService pAdESService = new PAdESService(cv);
+                            final String tspServer = "http://pki.codegic.com/codegic-service/timestamp";
+                            OnlineTSPSource tspSource = new OnlineTSPSource(tspServer);
+                            tspSource.setNonceSource(new SecureRandomNonceSource());
+                            tspSource.setPolicyOid("1.2.1");
+//                            tspSource.setPolicyOid("0.4.0.2023.1.1"); // NOOOOOOOT SUREEEEEEEEE
+
+                            pAdESService.setTspSource(tspSource);
+
+                            DSSDocument ltaLevelDocument = pAdESService.extendDocument(toExtendDocument, parameters);
+
+                            try (InputStream inputStream = ltaLevelDocument.openStream()) {
+                                byte[] contentBytes = new byte[inputStream.available()];
+                                inputStream.read(contentBytes);
+                                // Assuming content is text, you can convert it to a String
+//                                String contentAsString = new String(contentBytes);
+                                String contentAsString = Base64.getEncoder().encodeToString(contentBytes);
+
+                                PrintWriter writer = new PrintWriter("output.txt");
+                                writer.print(contentAsString);
+                                writer.close();
+
+//                                System.out.println("Content of the DSS Document: " + contentAsString);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else
+                            throw new Exception("Signature not valid for one object");
+                    }
+                }
+            }
+
+        }
+
+//        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        String username = userDetails.getUsername();
+//        return username;
+        return "alex";
     }
 
 
